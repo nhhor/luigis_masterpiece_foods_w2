1bf6e84c0d635206cf56c594e80165a1d5cbaba4
nhhor <nhhor@post.com>
2020-01-24 15:54:01 -0800

    update products_specs. Test cov at 85.42%

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -9,6 +9,39 @@ describe "Products" do
     expect(page).to have_content 'The record you tried to access no longer exists.'
   end
 
+  it "routes to New product page correctly" do
+    admin = FactoryBot.create(:admin)
+    login_as(admin)
+    visit '/'
+    click_link 'Products'
+    click_link 'Add new product'
+    expect(page).to have_content 'New product'
+  end
+
+  it "creates a new product" do
+    admin = FactoryBot.create(:admin)
+    login_as(admin)
+    visit '/products/new'
+    fill_in 'product_name', :with => 'Medjool Date'
+    fill_in 'product_cost', :with => 2.99
+    fill_in 'product_country_of_origin', :with => 'USA'
+    click_on 'Create Product'
+    expect(page).to have_content 'Product successfully created!'
+    expect(page).to have_content 'Medjool Date'
+  end
+
+  it "correctly handels an error creating a new product" do
+    admin = FactoryBot.create(:admin)
+    login_as(admin)
+    visit '/products/new'
+    # fill_in 'product_name', :with => ''
+    fill_in 'product_cost', :with => 1.99
+    fill_in 'product_country_of_origin', :with => 'Canada'
+    click_on 'Create Product'
+    expect(page).to have_content 'There was a problem creating this product!'
+    expect(page).to have_content "Name can't be blank"
+  end
+
 
 end
 
